964cb602709f7142ce684e04645e194c30ad4a7c
Jan Šándor <sandor.jan9@gmail.com>
2026-08-31 21:18:06 +0200

    feat: Update Footer year

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,11 @@
+const currentYear = new Date().getFullYear();
+
 export const Footer = () => {
   return (
     <footer className="bg-neutral-900 p-6 text-white">
-      <p className="text-center">&copy; 2026 Itinna. All rights reserved.</p>
+      <p className="text-center">
+        &copy; {currentYear} Itinna. All rights reserved.
+      </p>
     </footer>
   );
 };
