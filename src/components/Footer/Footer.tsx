70cc501f8f2024d13341137231206994120bbaa3
Jan Šándor <sandor.jan9@gmail.com>
2026-09-05 15:00:27 +0200

    feat: Remove footer background

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ const currentYear = new Date().getFullYear();
 
 export const Footer = () => {
   return (
-    <footer className="bg-neutral-900 p-6 text-white">
+    <footer className="p-6 text-white">
       <p className="text-small text-center">
         &copy; {currentYear} Itinna. All rights reserved.
       </p>
